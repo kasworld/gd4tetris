53ac9608177a8a367d5fe9811e6a2bc5c4a87ac0
SeukWon Kang <kasworld@gmail.com>
2023-06-21 10:22:22 +0900

    code clean

--- main.gd
+++ main.gd
@@ -261,6 +261,7 @@ class Tetromino extends Node2D:
 			var tu = board.new_tu(x+p[0],y+p[1], co)
 			tulist.append(tu)
 			board.add_child(tu)
+		board.draw_shadow_by(tulist)
 
 	func change_type(ta :int):
 		t = ta
@@ -345,9 +346,6 @@ var tet_board :Board
 var tet_mino_move :Tetromino
 var tet_mino_next :Tetromino
 
-func copy_tmino_move_from_next()->void:
-	tet_mino_move.copy_tmino_from_next(tet_board.BoardW/2-1,0,tet_mino_next)
-
 func _ready() -> void:
 	randomize()
 	var width = ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -368,19 +366,24 @@ func _ready() -> void:
 	$Score.position.x = boardWidth  #boardWidth + tet_board.board2screenW *2
 	$Score.position.y = tet_board.board2screenH *0
 
-	start_game()
-
-func start_game():
 	tet_mino_next = Tetromino.new(self, tet_board)
 	tet_mino_next.make_tmino(tet_board.BoardW+1,4,Tetromino.rand_type(),0)
 	tet_mino_move = Tetromino.new(self, tet_board)
-	copy_tmino_move_from_next()
+	proceed_next()
+	init_newgame()
+
+func proceed_next():
+	tet_mino_move.copy_tmino_from_next(tet_board.BoardW/2-1,0,tet_mino_next)
 	tet_mino_next.change_type(Tetromino.rand_type())
+
+func init_newgame():
+	force_down_frame = 60
+	since_last_force_down_frame = 0
 
 func game_over():
 	$GameOver.visible = true
 	tet_board.clear_board()
-
+	init_newgame()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -389,7 +392,7 @@ func _process(delta: float) -> void:
 	else:
 #		removelinetest()
 		handle_input()
-		force_down(delta)
+		force_down()
 
 	$Score.text = "%d" % tet_board.score
 	pass
@@ -406,18 +409,17 @@ func handle_input()->void:
 	if Input.is_action_just_pressed("hard_drop"):
 		tet_board.down_to_can(tet_mino_move.tulist)
 
-var force_down_sec = 0.5
-var sum_since_last_force_down = 0.0
-func force_down(delta:float)->void:
-	sum_since_last_force_down += delta
-	if sum_since_last_force_down > force_down_sec:
-		sum_since_last_force_down = 0.0
+var force_down_frame :int
+var since_last_force_down_frame :int
+func force_down()->void:
+	since_last_force_down_frame +=1
+	if since_last_force_down_frame > force_down_frame:
+		since_last_force_down_frame = 0
 		$GameOver.visible = false
 		var act_success = tet_mino_move.move_down()
 		if !act_success:
 			tet_board.set_to_board(tet_mino_move.tulist)
-			copy_tmino_move_from_next()
-			tet_mino_next.change_type(Tetromino.rand_type())
+			proceed_next()
 			if !tet_board.can_set_to_board(tet_mino_move.tulist):
 				game_over()
 
