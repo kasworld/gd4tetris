b3b3eb6ab7ff29db2f1b8f91084e4ad013189be5
SeukWon Kang <kasworld@gmail.com>
2023-06-21 11:39:24 +0900

    make game class

--- main.gd
+++ main.gd
@@ -37,6 +37,7 @@ class Board extends Node2D:
 	var del_effect = []
 	var shadow = []
 	var score :int
+
 	func new_shadow()->void:
 		for i in 4:
 			var o = new_tu(0,0,ShadowColor )
@@ -227,15 +228,14 @@ class Tetromino extends Node2D:
 		TypeJ: Color.MAGENTA,
 		TypeL: Color.ORANGE,
 	}
-	var scene : Node2D
 	var board : Board # board to check
 	var tulist = []
 	var x : int # x in board
 	var y: int  # y in board
 	var t: int  # tet type
 	var r: int  # rotate state
-	func _init(s :Node2D, b :Board)->void:
-		scene = s
+
+	func _init( b :Board)->void:
 		board = b
 
 	func make_tmino( xa :int,ya :int,ta :int,ra :int)->void:
@@ -342,104 +342,115 @@ class Tetromino extends Node2D:
 			return true
 		return false
 
-var tet_board :Board
-var tet_mino_move :Tetromino
-var tet_mino_next :Tetromino
+class Game extends Node2D:
+	var tet_board :Board
+	var tet_mino_move :Tetromino
+	var tet_mino_next :Tetromino
+	var force_down_frame :int
+	var since_last_force_down_frame :int
 
+	func _init()->void:
+		randomize()
+
+	func newgame()->void:
+		var width = ProjectSettings.get_setting("display/window/size/viewport_width")
+		var height =  ProjectSettings.get_setting("display/window/size/viewport_height")
+		var shift = Board.HiddenTop*(height / (Board.BoardH-Board.HiddenTop))
+		var boardWidth = width/3*2
+		var remainWidth = width - boardWidth
+		tet_board = Board.new(boardWidth,height + shift )
+		add_child(tet_board)
+		tet_board.position.y = -shift
+
+		var bgImage = Image.create(boardWidth,height,true,Image.FORMAT_RGBA8)
+		bgImage.fill(Color.BLACK)
+		var bgTexture = ImageTexture.create_from_image(bgImage)
+		get_parent().get_node("BGSprite2D").texture = bgTexture
+
+		get_parent().get_node("Score").size.x = remainWidth
+		get_parent().get_node("Score").position.x = boardWidth  #boardWidth + tet_board.board2screenW *2
+		get_parent().get_node("Score").position.y = tet_board.board2screenH *0
+
+		tet_mino_next = Tetromino.new(tet_board)
+		tet_mino_next.make_tmino(tet_board.BoardW+1,4,Tetromino.rand_type(),0)
+		tet_mino_move = Tetromino.new(tet_board)
+		proceed_next()
+		reset_game()
+
+	func proceed_next():
+		tet_mino_move.copy_tmino_from_next(tet_board.BoardW/2-1,0,tet_mino_next)
+		tet_mino_next.change_type(Tetromino.rand_type())
+
+	func reset_game():
+		force_down_frame = 60
+		since_last_force_down_frame = 0
+
+	func game_over():
+		get_parent().get_node("GameOver").visible = true
+		tet_board.clear_board()
+		reset_game()
+
+
+	func handle_input()->void:
+		if Input.is_action_just_pressed("move_right"):
+			tet_mino_move.move_right()
+		if Input.is_action_just_pressed("move_left"):
+			tet_mino_move.move_left()
+		if Input.is_action_pressed("move_down"):
+			tet_mino_move.move_down()
+		if Input.is_action_just_pressed("rotate"):
+			tet_mino_move.tmino_rotate()
+		if Input.is_action_just_pressed("hard_drop"):
+			tet_board.down_to_can(tet_mino_move.tulist)
+
+	func force_down()->void:
+		since_last_force_down_frame +=1
+		if since_last_force_down_frame > force_down_frame:
+			since_last_force_down_frame = 0
+			get_parent().get_node("GameOver").visible = false
+			var act_success = tet_mino_move.move_down()
+			if !act_success:
+				tet_board.set_to_board(tet_mino_move.tulist)
+				proceed_next()
+				if !tet_board.can_set_to_board(tet_mino_move.tulist):
+					game_over()
+
+	func process(delta: float) -> void:
+		if tet_board.start_remove_fulllines() :
+			tet_board.scroll_down_column()
+		else:
+	#		removelinetest()
+			handle_input()
+			force_down()
+
+		get_parent().get_node("Score").text = "%d" % tet_board.score
+		pass
+
+	func removelinetest()->void:
+		for i in range(tet_board.BoardW):
+			tet_board.add_tu_to_board(
+				tet_board.rand_x(),tet_board.rand_y(),
+				Tetromino.TetColor[ Tetromino.rand_type()]
+				)
+
+	func act_random()->void:
+		var act = randi_range(0,4)
+		match act:
+			0: # rotate
+				tet_mino_move.tmino_rotate()
+			1: # left
+				tet_mino_move.move_left()
+			2: # right
+				tet_mino_move.move_right()
+			3: # down
+				tet_mino_move.move_down()
+
+var game :Game
 func _ready() -> void:
-	randomize()
-	var width = ProjectSettings.get_setting("display/window/size/viewport_width")
-	var height =  ProjectSettings.get_setting("display/window/size/viewport_height")
-	var shift = Board.HiddenTop*(height / (Board.BoardH-Board.HiddenTop))
-	var boardWidth = width/3*2
-	var remainWidth = width - boardWidth
-	tet_board = Board.new(boardWidth,height + shift )
-	add_child(tet_board)
-	tet_board.position.y = -shift
-
-	var bgImage = Image.create(boardWidth,height,true,Image.FORMAT_RGBA8)
-	bgImage.fill(Color.BLACK)
-	var bgTexture = ImageTexture.create_from_image(bgImage)
-	$BGSprite2D.texture = bgTexture
-
-	$Score.size.x = remainWidth
-	$Score.position.x = boardWidth  #boardWidth + tet_board.board2screenW *2
-	$Score.position.y = tet_board.board2screenH *0
-
-	tet_mino_next = Tetromino.new(self, tet_board)
-	tet_mino_next.make_tmino(tet_board.BoardW+1,4,Tetromino.rand_type(),0)
-	tet_mino_move = Tetromino.new(self, tet_board)
-	proceed_next()
-	init_newgame()
-
-func proceed_next():
-	tet_mino_move.copy_tmino_from_next(tet_board.BoardW/2-1,0,tet_mino_next)
-	tet_mino_next.change_type(Tetromino.rand_type())
-
-func init_newgame():
-	force_down_frame = 60
-	since_last_force_down_frame = 0
-
-func game_over():
-	$GameOver.visible = true
-	tet_board.clear_board()
-	init_newgame()
+	game = Game.new()
+	add_child(game)
+	game.newgame()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if tet_board.start_remove_fulllines() :
-		tet_board.scroll_down_column()
-	else:
-#		removelinetest()
-		handle_input()
-		force_down()
-
-	$Score.text = "%d" % tet_board.score
-	pass
-
-func handle_input()->void:
-	if Input.is_action_just_pressed("move_right"):
-		tet_mino_move.move_right()
-	if Input.is_action_just_pressed("move_left"):
-		tet_mino_move.move_left()
-	if Input.is_action_pressed("move_down"):
-		tet_mino_move.move_down()
-	if Input.is_action_just_pressed("rotate"):
-		tet_mino_move.tmino_rotate()
-	if Input.is_action_just_pressed("hard_drop"):
-		tet_board.down_to_can(tet_mino_move.tulist)
-
-var force_down_frame :int
-var since_last_force_down_frame :int
-func force_down()->void:
-	since_last_force_down_frame +=1
-	if since_last_force_down_frame > force_down_frame:
-		since_last_force_down_frame = 0
-		$GameOver.visible = false
-		var act_success = tet_mino_move.move_down()
-		if !act_success:
-			tet_board.set_to_board(tet_mino_move.tulist)
-			proceed_next()
-			if !tet_board.can_set_to_board(tet_mino_move.tulist):
-				game_over()
-
-############# test functions
-
-func removelinetest()->void:
-	for i in range(tet_board.BoardW):
-		tet_board.add_tu_to_board(
-			tet_board.rand_x(),tet_board.rand_y(),
-			Tetromino.TetColor[ Tetromino.rand_type()]
-			)
-
-func act_random()->void:
-	var act = randi_range(0,4)
-	match act:
-		0: # rotate
-			tet_mino_move.tmino_rotate()
-		1: # left
-			tet_mino_move.move_left()
-		2: # right
-			tet_mino_move.move_right()
-		3: # down
-			tet_mino_move.move_down()
+	game.process(delta)
